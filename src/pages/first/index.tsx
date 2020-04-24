3cde4fa6615f0e91679e4d80b43b0de1b4298d5c
zdp <zengdp0516@163.com>
2020-04-24 09:34:26 +0800

    通知

--- src/pages/first/index.tsx
+++ src/pages/first/index.tsx
@@ -1,7 +1,7 @@
 import Taro, { Component, Config } from '@tarojs/taro'
-import { View, Text, Image } from '@tarojs/components'
+import { View, Text, Image, Button } from '@tarojs/components'
 import './index.scss'
-import { AtButton, AtCard, AtTabBar, AtList, AtListItem } from 'taro-ui'
+import { AtButton, AtCard, AtNoticebar, AtList, AtListItem, AtModal, AtModalHeader, AtModalContent, AtModalAction } from 'taro-ui'
 import { records, getImgUrl, dateUtil } from '../../utils/static'
 const httpUtil = require('../../utils/httpUtil')
 interface IProps {
@@ -12,7 +12,11 @@ interface Istate {
   schoolList?: any,
   timeLineArr?: any,
   skillList?: any,
-  workList?: any
+  workList?: any,
+  noticeString: string,
+  columnArr?: any,
+  noticeModalVisible?: boolean,
+  noticeModalData?: boolean
 }
 export default class First extends Component<IProps, Istate> {
   constructor(props) {
@@ -22,7 +26,11 @@ export default class First extends Component<IProps, Istate> {
       schoolList: [],
       timeLineArr: [],
       skillList: [],
-      workList: []
+      workList: [],
+      noticeString: '',
+      columnArr: [],
+      noticeModalVisible: true,
+      noticeModalData: false
     }
   }
   /**
@@ -36,17 +44,62 @@ export default class First extends Component<IProps, Istate> {
     navigationBarTitleText: '预览简历'
   }
 
-  componentWillMount() { }
+  componentWillMount() {
+    const notice = Taro.getStorageSync('noticeModal')
+    console.log('notice', notice)
+    if (notice) {
+      console.log('aaa', notice)
+      this.setState({
+        noticeModalVisible: false
+      })
+    }
+  }
 
   componentDidMount() {
     const user = Taro.getStorageSync('userInfo')
+
     if (user) {
-      this.getResumeInfo()
-      this.getResumeSchool()
-      this.getResumeCompany()
-      this.getResumeSkill()
-      this.getResumeArtWork()
+      this.getResumeNotice()
     }
+  }
+  // 获取简历的通知信息
+  getResumeNotice = () => {
+    const that = this
+    httpUtil.request({
+      url: '/notice/list',
+      method: 'GET',
+      success(res) {
+        that.getResumeInfo()
+        that.getResumeSchool()
+        that.getResumeCompany()
+        that.getResumeSkill()
+        that.getResumeArtWork()
+        if (res.data) {
+          const noticeArr = res.data
+          let rowArr = []//滚动的通知
+          let columnArr = []//弹框的通知
+          noticeArr.map((item: any) => {
+            if (item.noticeType == '1') {
+              columnArr.push(item)
+            } else {
+              rowArr.push(item.noticeContent)
+            }
+          })
+          if (rowArr.length > 0) {
+            let st = ''
+            rowArr.map((item, index) => {
+              const order = index + 1
+              st = st + order + '，' + item
+            })
+            that.setState({ noticeString: st })
+          }
+          if (columnArr.length > 0) {
+            that.setState({ noticeModalData: true })
+          }
+          that.setState({ columnArr: columnArr })
+        }
+      }
+    })
   }
   // 获取简历的用户信息
   getResumeInfo = () => {
@@ -152,6 +205,13 @@ export default class First extends Component<IProps, Istate> {
       }
     })
   }
+  //通知的确定 
+  modalOk = () => {
+    Taro.setStorageSync('noticeModal', true)
+    this.setState({
+      noticeModalVisible: false
+    })
+  }
   componentWillUnmount() { }
 
   componentDidShow() { }
@@ -167,7 +227,7 @@ export default class First extends Component<IProps, Istate> {
     Taro.navigateTo({ url: `/pages/resume/skillList/skillList` })
   }
   render() {
-    const { resumeInfo, schoolList, timeLineArr, skillList, workList } = this.state
+    const { resumeInfo, schoolList, timeLineArr, skillList, workList, noticeString, noticeModalVisible, columnArr, noticeModalData } = this.state
     // console.log('resume', workList)
     const time = timeLineArr.map((item, index) => {
       return (
@@ -199,120 +259,139 @@ export default class First extends Component<IProps, Istate> {
       )
     })
     return (
-      <View className='wrapper'>
-        {Object.keys(resumeInfo).length > 0 ? <View>
-          <View className='fist-card'>
-            <View className='user-info'>
-              <View className='name'>{resumeInfo.realName}</View>
-              <View className='others'>
-                <View>{resumeInfo.sex == '0' ? '男' : '女'}</View>
-                <View>{resumeInfo.mobile}</View>
-                <View>{resumeInfo.city}</View>
-                <View>{dateUtil(resumeInfo.birthday)}岁</View>
+      <View>
+         {/* 系统的通知提醒 */}
+         <AtModal isOpened={noticeModalVisible && noticeModalData} closeOnClickOverlay={false}>
+            <AtModalHeader>{columnArr[0].noticeTitle}</AtModalHeader>
+            <AtModalContent>
+              <View className="modal-content">
+                <View>{columnArr[0].noticeContent}</View>
+              </View>
+            </AtModalContent>
+            <AtModalAction><Button onClick={this.modalOk}>确定</Button> </AtModalAction>
+          </AtModal>
+          {noticeString == '' ? '' :
+            <View className='notice'>
+              <AtNoticebar icon='volume-plus' marquee>
+                {noticeString}
+              </AtNoticebar>
+            </View>
+          }
+        <View className='wrapper'>
+          {Object.keys(resumeInfo).length > 0 ? <View>
+            <View className='fist-card'>
+              <View className='user-info'>
+                <View className='name'>{resumeInfo.realName}</View>
+                <View className='others'>
+                  <View>{resumeInfo.sex == '0' ? '男' : '女'}</View>
+                  <View>{resumeInfo.mobile}</View>
+                  <View>{resumeInfo.city}</View>
+                  <View>{dateUtil(resumeInfo.birthday)}岁</View>
+                </View>
               </View>
             </View>
-          </View>
-          <View className='item'>
-            <AtCard title='求职期望' className='card-model'>
-              <View className='expect'>
-                <View className='post'>
-                  <Text>{resumeInfo.province}</Text>,<Text>{resumeInfo.city}</Text>
+            <View className='item'>
+              <AtCard title='求职期望' className='card-model'>
+                <View className='expect'>
+                  <View className='post'>
+                    <Text>{resumeInfo.province}</Text>,<Text>{resumeInfo.city}</Text>
+                  </View>
+                  <View>
+                    {resumeInfo.salary}
+                  </View>
                 </View>
-                <View>
-                  {resumeInfo.salary}
-                </View>
-              </View>
-            </AtCard>
-          </View>
-        </View> : ''}
-
-        {
-          schoolList.length > 0 ? <View className='item'>
-            <AtCard title='教育经历'>
-              <AtList hasBorder={false}>
-                {
-                  schoolList.map((item, index) => {
-                    return (
-                      <AtListItem
-                        arrow='right'
-                        title={item.schoolName}
-                        extraText={item.projectName}
-                        note={records[item.record]}
-                        onClick={() => this.goToSchoolInfo(item)}
-                        key={item.id}
-                      />
-                    )
-                  })
-                }
-              </AtList>
-            </AtCard>
-          </View> : ''
-        }
-        {
-          timeLineArr.length > 0 ? <View className='item'>
-            <AtCard
-              title='工作经历'>
-
-              <View className='timeLine'>
-                {time}
-              </View>
-            </AtCard>
-          </View> : ''
-        }
-
-        {
-          skillList.length > 0 ? <View className='item'>
-            <AtCard title='个人技能'>
-              <AtList hasBorder={false}>
-                <View onClick={this.gotoSkillList}>
-                  {skillList.map((item, index) => {
-                    let targe
-                    if (item.skillGrade == 0) {
-                      targe = <View className='skill-tag tag-one' key={item.id}>{item.skillName}</View>
-                    } else if (item.skillGrade == 1) {
-                      targe = <View className='skill-tag tag-two' key={item.id}>{item.skillName}</View>
-                    } else if (item.skillGrade == 2) {
-                      targe = <View className='skill-tag tag-three' key={item.id}>{item.skillName}</View>
-                    } else if (item.skillGrade == 3) {
-                      targe = <View className='skill-tag tag-four' key={item.id}>{item.skillName}</View>
-                    } else if (item.skillGrade == 4) {
-                      targe = <View className='skill-tag tag-five' key={item.id}>{item.skillName}</View>
-                    }
-                    return (
-                      targe
-                    )
-                  })}
-                </View>
-              </AtList>
-            </AtCard>
-          </View> : ''
-        }
-
-        {
-          workList.length > 0 ? <View className='item'>
-            <AtCard title='个人作品'>
-              <View className='art'>
-                {
-                  workList.map((item, index) => {
-                    return (
-                      <View className='art-item' onClick={() => { this.cloneArtWordUrl(item) }} key={item.id}>
-                        <Image className='item-img' src={getImgUrl(item.imgType)}></Image>
-                        <View className='item-title'>{item.workName}</View>
-                      </View>
-                    )
-                  })
-                }
-              </View>
-            </AtCard>
-            <View className='bottomBtn'>
-              <AtButton openType='share' circle type="primary">分享我的简历</AtButton>
+              </AtCard>
             </View>
-          </View> : ''
-        }
-        {
-          Object.keys(resumeInfo).length < 1 ? <View className='empty'>简历待完善！</View> : ''
-        }
+          </View> : ''}
 
+          {
+            schoolList.length > 0 ? <View className='item'>
+              <AtCard title='教育经历'>
+                <AtList hasBorder={false}>
+                  {
+                    schoolList.map((item, index) => {
+                      return (
+                        <AtListItem
+                          arrow='right'
+                          title={item.schoolName}
+                          extraText={item.projectName}
+                          note={records[item.record]}
+                          onClick={() => this.goToSchoolInfo(item)}
+                          key={item.id}
+                        />
+                      )
+                    })
+                  }
+                </AtList>
+              </AtCard>
+            </View> : ''
+          }
+          {
+            timeLineArr.length > 0 ? <View className='item'>
+              <AtCard
+                title='工作经历'>
+
+                <View className='timeLine'>
+                  {time}
+                </View>
+              </AtCard>
+            </View> : ''
+          }
+
+          {
+            skillList.length > 0 ? <View className='item'>
+              <AtCard title='个人技能'>
+                <AtList hasBorder={false}>
+                  <View onClick={this.gotoSkillList}>
+                    {skillList.map((item, index) => {
+                      let targe
+                      if (item.skillGrade == 0) {
+                        targe = <View className='skill-tag tag-one' key={item.id}>{item.skillName}</View>
+                      } else if (item.skillGrade == 1) {
+                        targe = <View className='skill-tag tag-two' key={item.id}>{item.skillName}</View>
+                      } else if (item.skillGrade == 2) {
+                        targe = <View className='skill-tag tag-three' key={item.id}>{item.skillName}</View>
+                      } else if (item.skillGrade == 3) {
+                        targe = <View className='skill-tag tag-four' key={item.id}>{item.skillName}</View>
+                      } else if (item.skillGrade == 4) {
+                        targe = <View className='skill-tag tag-five' key={item.id}>{item.skillName}</View>
+                      }
+                      return (
+                        targe
+                      )
+                    })}
+                  </View>
+                </AtList>
+              </AtCard>
+            </View> : ''
+          }
+
+          {
+            workList.length > 0 ? <View className='item'>
+              <AtCard title='个人作品'>
+                <View className='art'>
+                  {
+                    workList.map((item, index) => {
+                      return (
+                        <View className='art-item' onClick={() => { this.cloneArtWordUrl(item) }} key={item.id}>
+                          <Image className='item-img' src={getImgUrl(item.imgType)}></Image>
+                          <View className='item-title'>{item.workName}</View>
+                        </View>
+                      )
+                    })
+                  }
+                </View>
+              </AtCard>
+              <View className='bottomBtn'>
+                <AtButton openType='share' circle type="primary">分享我的简历</AtButton>
+              </View>
+            </View> : ''
+          }
+          {
+            Object.keys(resumeInfo).length < 1 ? <View className='empty'>简历待完善！</View> : ''
+          }
+
+        </View>
       </View>
     )
   }
